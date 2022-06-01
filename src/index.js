1095afb9f0e58379ca8a826e1061ee3ebaa0ecf4
renanshin <renanshin@gmail.com>
2022-06-01 20:11:45 -0300

    Removido shimmer conflito

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Shimmer from 'react-native-shimmer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Shimmer>
-    <Text>Loading...</Text>
-    </Shimmer>;
   </React.StrictMode>
 );
 
